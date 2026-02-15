ecce5f725b3c6424bbf20a0a0e918d23159200ef
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-02-15 18:20:20 +0530

    variable usage in the script

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="RAM" #no space betweeen before and after = sign
+PERSON2="SAM"
+
+echo "${PERSON1} :: Hi $PERSON2"
+echo "$PERSON2 :: Hello $PERSON1"
+echo "$PERSON1 :: How are you doing?"
+echo "$PERSON2 :: I am Good How are You?"

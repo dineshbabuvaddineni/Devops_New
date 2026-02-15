427845e769db4684fbe4847646597c1c5f7c7c50
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-02-15 18:48:04 +0530

    uservariables

---
+++ 05_Variables_UserInput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#User input variables for reading username and password
+
+echo "Enter your username::"
+read USERNAME
+echo "Your username is :: $USERNAME"
+echo "Enter your Password::"
+read -s PASSWORD
+

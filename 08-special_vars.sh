564bfea0f86cf0d8ffdac4c00ba7d48c1f7b146f
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-02-16 23:22:41 +0530

    special variables

---
+++ 08-special_vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All Variables passed : $@"
+echo "number os variables passed $#"
+echo "Script Name : $0"
+echo "present working directory :$PWD"
+echo " Which user is running this script : $user"
+echo "process id of current script : $$"
+sleep 60 &
+echo "process id of last command in background :$!"

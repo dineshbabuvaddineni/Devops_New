17aed9793183f52eb145bb49493400147fa1b0c5
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-02-18 22:23:58 +0530

    Install Script_3

--- 10-Install_Script.sh
+++ 10-Install_Script.sh
@@ -2,8 +2,10 @@
 
 USERID=$(id -u)
 
-if[ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "ERROR :: You must have sudo access to execute the script"
+    exit 1
 fi
-dnf install mysql -y 
+
+dnf install mysql -y

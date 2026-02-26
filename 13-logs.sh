165e2b0a662cd55141c9a9d900609c786c29fa6d
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-02-27 00:00:30 +0530

    logs4

--- 13-logs.sh
+++ 13-logs.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R='\e[31m' # Red]
+
+R='\e[31m' # Red
 G='\e[32m' # Green
 Y='\e[33m' # Yellow
-N='\e[0m' # No colour
+N='\e[0m'  # No colour
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
+LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME= "$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+# Create log folder if not exists
+#mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e     "$2 .....$R FAILURE $N"
+        echo -e "$2 ..... $R FAILURE $N"
         exit 1
     else
         echo -e "$2 ..... $G SUCCESS $N"
     fi
-
 }
 
-echo "script started executing at $TIMESTAMP &>>$LOG_FILE_NAME" 
+echo "Script started executing at $TIMESTAMP" &>> $LOG_FILE_NAME
 
 if [ $USERID -ne 0 ]
 then
@@ -30,20 +33,22 @@ then
     exit 1
 fi
 
-dnf list installed mysql &>>$LOG_FILE_NAME
+# MYSQL
+dnf list installed mysql &>> $LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install mysql -y &>>$LOG_FILE_NAME
+    dnf install mysql -y &>> $LOG_FILE_NAME
     VALIDATE $? "Installing MYSQL"
 else
-    echo -e "MYSQL is already .....$Y Installed $N"
+    echo -e "MYSQL is already ..... $Y Installed $N"
 fi
 
-dnf list installed git
+# GIT
+dnf list installed git &>> $LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install git -y &>>$LOG_FILE_NAME
+    dnf install git -y &>> $LOG_FILE_NAME
     VALIDATE $? "Installing git"
 else
-    echo -e "git is already ....$Y Installed $N"
+    echo -e "git is already ..... $Y Installed $N"
 fi

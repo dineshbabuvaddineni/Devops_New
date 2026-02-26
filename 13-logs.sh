6c7ed273393a576ef43482dc8352170dd3556639
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-02-26 23:40:03 +0530

    logs1

--- 13-logs.sh
+++ 13-logs.sh
@@ -21,7 +21,7 @@ VALIDATE(){
 
 }
 
-echo "script started executing at $TIMESTAMP &>>$LOG_FILE" 
+echo "script started executing at $TIMESTAMP &>>$LOG_FILE_NAME" 
 
 if [ $USERID -ne 0 ]
 then
@@ -29,7 +29,7 @@ then
     exit 1
 fi
 
-dnf list installed mysql &>>$LOG_FILE
+dnf list installed mysql &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
     dnf install mysql -y &>>$LOG_FILE
@@ -41,7 +41,7 @@ fi
 dnf list installed git
 if [ $? -ne 0 ]
 then
-    dnf install git -y &>>$LOG_FILE
+    dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing git"
 else
     echo -e "git is already ....$Y Installed"

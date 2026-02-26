4871e6b68870cef08dc266b2ce4a0e3d6fe2f3fb
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-02-26 23:51:18 +0530

    logs3

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,19 +4,20 @@ USERID=$(id -u)
 R='\e[31m' # Red]
 G='\e[32m' # Green
 Y='\e[33m' # Yellow
+N='\e[0m' # No colour
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME= "$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e     "$2 .....$R FAILURE"
+        echo -e     "$2 .....$R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ..... $G SUCCESS"
+        echo -e "$2 ..... $G SUCCESS $N"
     fi
 
 }
@@ -32,10 +33,10 @@ fi
 dnf list installed mysql &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install mysql -y &>>$LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing MYSQL"
 else
-    echo -e "MYSQL is already .....$Y Installed"
+    echo -e "MYSQL is already .....$Y Installed $N"
 fi
 
 dnf list installed git
@@ -44,5 +45,5 @@ then
     dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing git"
 else
-    echo -e "git is already ....$Y Installed"
+    echo -e "git is already ....$Y Installed $N"
 fi

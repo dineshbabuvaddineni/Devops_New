03f56e4ecc5ef41cc23e889d2a4a4eb2bbc44ef6
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-03-03 22:10:45 +0530

    loops2

---
+++ 15-loops.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R='\e[31m' # Red
+G='\e[32m' # Green
+Y='\e[33m' # Yellow
+N='\e[0m'  # No colour
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+# Create log folder if not exists
+#mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ..... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ..... $G SUCCESS $N"
+    fi
+}
+
+echo "Script started executing at $TIMESTAMP" &>> $LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR :: You must have sudo access to execute the script"
+    exit 1
+fi
+
+
+
+# MYSQL
+for package in $@
+do
+    dnf list installed $package &>> $LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        dnf install $package -y &>> $LOG_FILE_NAME
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already $Y ..... Installed $N"
+    fi
+done
